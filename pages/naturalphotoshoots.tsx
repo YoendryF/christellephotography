4de887c055f512fdd59e0ba987aaaed057edc3ce
Yoendry <yoendryferro2003@gmail.com>
2025-06-29 17:14:00 -0400

    Fix image filename case issues

--- pages/naturalphotoshoots.tsx
+++ pages/naturalphotoshoots.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 export default function NaturePhotosPage() {
     const naturePhotos = [
-      "Nature/Nature1.JPG", "Nature/Nature2.JPEG", "Nature/Nature3.JPG", "Nature/Nature4.JPG","Nature/Nature5.JPG","Nature/Nature6.JPEG","Nature/Nature7.JPG","Nature/Nature8.JPG","Nature/Nature9.JPG","Nature/Nature10.JPG","Nature/Nature11.JPG","Nature/Nature12.JPG","Nature/Nature13.JPEG","Nature/Nature14.JPEG","Nature/Nature15.JPG","Nature/Nature16.JPEG","Nature/Nature17.JPG","Nature/Nature18.JPG","Nature/Nature19.JPG"
+      "../Nature/Nature1.JPG", "../Nature/Nature2.JPEG", "../Nature/Nature3.JPG", "../Nature/Nature4.JPG","../Nature/Nature5.JPG","../Nature/Nature6.JPEG","../Nature/Nature7.JPG","../Nature/Nature8.JPG","../Nature/Nature9.JPG","../Nature/Nature10.JPG","../Nature/Nature11.JPG","../Nature/Nature12.JPG","../Nature/Nature13.JPEG","../Nature/Nature14.JPEG","../Nature/Nature15.JPG","../Nature/Nature16.JPEG","../Nature/Nature17.JPG","../Nature/Nature18.JPG","../Nature/Nature19.JPG"
     ];
   
     return (
--- pages/portraits.tsx
+++ pages/portraits.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 export default function PortraitsPage() {
     const portraits = [
-      "Portraits/Portrait1.JPG", "Portraits/Portrait2.JPG", "Portraits/Portrait3.JPG", "Portraits/Portrait4.JPG", "Portraits/Portrait5.JPG", "Portraits/Portrait6.JPG", "Portraits/Portrait7.JPG", "Portraits/Portrait8.JPG", "Portraits/Portrait9.JPG", "Portraits/Portrait10.JPEG", "Portraits/Portrait11.JPEG","Portraits/Portrait12.JPG","Portraits/Portrait13.JPG","Portraits/Portrait14.JPG","Portraits/Portrait15.JPG","Portraits/Portrait16.JPG","Portraits/Portrait17.JPG"
+      "../Portraits/Portrait1.JPG", "../Portraits/Portrait2.JPG", "../Portraits/Portrait3.JPG", "../Portraits/Portrait4.JPG", "../Portraits/Portrait5.JPG", "../Portraits/Portrait6.JPG", "../Portraits/Portrait7.JPG", "../Portraits/Portrait8.JPG", "../Portraits/Portrait9.JPG", "../Portraits/Portrait10.JPEG", "../Portraits/Portrait11.JPEG","../Portraits/Portrait12.JPG","../Portraits/Portrait13.JPG","../Portraits/Portrait14.JPG","../Portraits/Portrait15.JPG","../Portraits/Portrait16.JPG","../Portraits/Portrait17.JPG"
     ];
   
     return (
