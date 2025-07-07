8c4f2b578d01b59badecdcf8a7462fe01430e03e
Yoendry <yoendryferro2003@gmail.com>
2025-07-07 17:02:32 -0400

    Adding polaroid frames and adjusting height size

---
+++ docs/_next/static/chunks/app/layout-c2fb0b8c2d1c6a55.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6395:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7105,23))},7105:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[576,347,870,358],()=>s(6395)),_N_E=e.O()}]);
