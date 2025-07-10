ade6f58397c493ffcd97962b441ca79e593687d8
Yoendry <yoendryferro2003@gmail.com>
2025-07-09 20:24:57 -0400

    removing small pictures

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,23 +20,23 @@ export default function Home() {
   const polaroids = [
     {
       caption: "Headshots",
-      images: ["/Headshots/Headshot1.JPG","/Headshots/Headshot2.JPEG","/Headshots/Headshot3.JPG","/Headshots/Headshot4.JPG","/Headshots/Headshot5.JPEG","/Headshots/Headshot6.JPEG","/Headshots/Headshot7.JPEG","/Headshots/Headshot8.JPEG","/Headshots/Headshot9.JPEG","/Headshots/Headshot10.JPEG","/Headshots/Headshot11.JPEG","/Headshots/Headshot12.JPEG"]
+      images: ["/Headshots/Headshot1.JPG","/Headshots/Headshot4.JPG","/Headshots/Headshot5.JPEG","/Headshots/Headshot8.JPEG","/Headshots/Headshot9.JPEG","/Headshots/Headshot10.JPEG","/Headshots/Headshot11.JPEG",]
     },
     {
       caption: "Grad Pics",
-      images: ["/Grad/Grad1.JPG","/Grad/Grad2.JPG","/Grad/Grad3.JPG","/Grad/Grad4.JPEG","/Grad/Grad5.JPEG","/Grad/Grad6.JPG","/Grad/Grad7.JPG","/Grad/Grad8.JPEG","/Grad/Grad9.JPEG","/Grad/Grad10.JPEG","/Grad/Grad11.JPEG","/Grad/Grad12.JPEG","/Grad/Grad13.JPG","/Grad/Grad14.JPG","/Grad/Grad15.JPG","/Grad/Grad16.JPG","/Grad/Grad17.JPG","/Grad/Grad18.JPG"],
+      images: ["/Grad/Grad1.JPG","/Grad/Grad2.JPG","/Grad/Grad3.JPG","/Grad/Grad4.JPEG","/Grad/Grad5.JPEG","/Grad/Grad6.JPG","/Grad/Grad7.JPG","/Grad/Grad8.JPEG","/Grad/Grad9.JPEG","/Grad/Grad10.JPEG","/Grad/Grad11.JPEG","/Grad/Grad12.JPEG","/Grad/Grad13.JPG","/Grad/Grad14.JPG","/Grad/Grad15.JPG","/Grad/Grad16.JPG","/Grad/Grad17.JPG","/Grad/Grad18.JPG"]
     },
     {
       caption: "Natural Photoshoots",
-      images: ["/Nature/Nature1.JPG", "/Nature/Nature2.JPEG", "/Nature/Nature3.JPG", "/Nature/Nature4.JPG","/Nature/Nature5.JPG","/Nature/Nature6.JPEG","/Nature/Nature7.JPG","/Nature/Nature8.JPG","/Nature/Nature9.JPG","/Nature/Nature10.JPG","/Nature/Nature11.JPG","/Nature/Nature12.JPG","/Nature/Nature13.JPEG","/Nature/Nature14.JPEG","/Nature/Nature16.JPEG","/Nature/Nature17.JPG","/Nature/Nature18.JPG","/Nature/Nature19.JPG","/Nature/Nature20.JPEG","/Nature/Nature21.JPG","/Nature/Nature22.JPG","/Nature/Nature23.JPG","/Nature/Nature24.JPG","/Nature/Nature25.JPG","/Nature/Nature26.JPG","/Nature/Nature27.JPG","/Nature/Nature28.JPG","/Nature/Nature29.JPG","/Nature/Nature30.JPEG"]
+      images: ["/Nature/Nature1.JPG", "/Nature/Nature4.JPG","/Nature/Nature5.JPG","/Nature/Nature8.JPG","/Nature/Nature9.JPG","/Nature/Nature10.JPG","/Nature/Nature11.JPG","/Nature/Nature12.JPG","/Nature/Nature13.JPEG","/Nature/Nature16.JPEG","/Nature/Nature17.JPG","/Nature/Nature18.JPG","/Nature/Nature19.JPG","/Nature/Nature20.JPEG","/Nature/Nature21.JPG","/Nature/Nature22.JPG","/Nature/Nature23.JPG","/Nature/Nature24.JPG","/Nature/Nature25.JPG","/Nature/Nature26.JPG","/Nature/Nature27.JPG","/Nature/Nature28.JPG","/Nature/Nature29.JPG","/Nature/Nature30.JPEG"]
     },
     {
       caption: "Portraits",
-      images: ["/Portraits/Portrait1.JPG", "/Portraits/Portrait2.JPG", "/Portraits/Portrait3.JPG","/Portraits/Portrait4.JPG","/Portraits/Portrait5.JPG","/Portraits/Portrait7.JPG","/Portraits/Portrait8.JPG","/Portraits/Portrait9.JPG","/Portraits/Portrait10.JPEG","/Portraits/Portrait11.JPEG","/Portraits/Portrait12.JPG","/Portraits/Portrait13.JPG","/Portraits/Portrait14.JPG","/Portraits/Portrait15.JPG","/Portraits/Portrait16.JPG","/Portraits/Portrait17.JPG","/Portraits/Portrait18.JPG","/Portraits/Portrait19.JPG","/Portraits/Portrait20.JPG","/Portraits/Portrait21.JPG","/Portraits/Portrait22.JPEG","/Portraits/Portrait23.JPEG","/Portraits/Portrait24.JPG","/Portraits/Portrait25.JPG"]
+      images: ["/Portraits/Portrait1.JPG", "/Portraits/Portrait2.JPG", "/Portraits/Portrait3.JPG","/Portraits/Portrait4.JPG","/Portraits/Portrait5.JPG","/Portraits/Portrait7.JPG","/Portraits/Portrait8.JPG","/Portraits/Portrait9.JPG","/Portraits/Portrait10.JPEG","/Portraits/Portrait11.JPEG","/Portraits/Portrait12.JPG","/Portraits/Portrait13.JPG","/Portraits/Portrait14.JPG","/Portraits/Portrait16.JPG","/Portraits/Portrait17.JPG","/Portraits/Portrait18.JPG","/Portraits/Portrait19.JPG","/Portraits/Portrait20.JPG","/Portraits/Portrait21.JPG","/Portraits/Portrait22.JPEG","/Portraits/Portrait23.JPEG","/Portraits/Portrait24.JPG","/Portraits/Portrait25.JPG"]
     },
     {
       caption: "Themed Photoshoots",
-      images: ["/Themed/Themed1.JPG", "/Themed/Themed2.JPG", "/Themed/Themed3.JPEG","/Themed/Themed4.JPG","/Themed/Themed5.JPG","/Themed/Themed6.JPEG","/Themed/Themed7.JPG","/Themed/Themed8.JPG"]
+      images: ["/Themed/Themed1.JPG", "/Themed/Themed2.JPG", "/Themed/Themed3.JPEG","/Themed/Themed4.JPG","/Themed/Themed5.JPG","/Themed/Themed6.JPEG","/Themed/Themed7.JPG"]
     }
   ];
 
